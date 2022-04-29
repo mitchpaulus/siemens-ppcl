73a8292c58fbec969ccadfc9359e4beaabea194e
Mitchell Paulus <paulusm14@gmail.com>
2022-04-29 16:59:02 -0500

    Fix very very bad recursion
    Not sure how this was even working at all before. At every depth of the
    call stack, the accumulator list was adding itself. That's exponential
    growth at its finest. Ran it all the way to List overflow.

--- Siemens-PPCL/Program.cs
+++ Siemens-PPCL/Program.cs
@@ -142,7 +142,7 @@ public class Program
             }
             else
             {
-                currentList.AddRange(AllPoints(child, currentList));
+                AllPoints(child, currentList);
             }
         }
 
